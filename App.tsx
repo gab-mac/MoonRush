3443f1f1308ff7de06cd03958b4c0b1a02b38cda
gabriel <gabriel-machado@outlook.com.br>
2022-01-19 13:48:08 -0300

    feat: add navigation bar buttons

--- App.tsx
+++ App.tsx
@@ -1,18 +1,25 @@
 import React from 'react';
-import { Text, View } from 'react-native';
+import { StyleSheet, Text, View } from 'react-native';
+import NavBar from './src/components/navBar';
 
 const App = () => {
   return (
     <>
-      <View style={{
-        flex: 1,
-        justifyContent: 'center',
-        alignItems: 'center'
-      }}>
+      <View style={styles.background}>
         <Text>Hello, world!</Text>
       </View>
+      <NavBar />
     </>
   );
 }
+
+const styles = StyleSheet.create({
+  background: {
+    flex: 1,
+    justifyContent: 'center',
+    alignItems: 'center',
+    backgroundColor: 'blue'
+  }
+})
 
 export default App;
